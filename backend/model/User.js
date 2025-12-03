2500216667dd3bf7edc1498f45a3bd84b9d46a92
saket mishra <mishrasaket042@gmail.com>
2025-12-04 01:43:33 +0530

    update

---
+++ backend/model/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const passportLocalMongoose=require("passport-local-mongoose");
+
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Your email address is required"],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Your username is required"],
+  },
+});
+
+//
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
